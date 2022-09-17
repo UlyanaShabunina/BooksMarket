f99d5ad20cb7679668ece22bcbc20ed53cebdf5e
cheryyjuicee <r.voroncova@mail.ru>
2022-09-17 15:43:13 +0300

    add

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 ##app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mbiutgxizuacil:6f1d5a81503b6d5aa343ecd1876b47fd2fdbaf4780f83ac2c89ad6304cccb0d5@ec2-54-85-56-210.compute-1.amazonaws.com:5432/d3kqos7ufokc54'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yzjtxjpbleszqa:18cb604636666e6e5c1e1209f4c0c367bad128e1cc9387d20d5021e2c9c32509@ec2-3-229-165-146.compute-1.amazonaws.com:5432/d12gel4qhc7rjm'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'xf3g82g27f9cdv443ui480nw'
